d5d76889239d3ee8e2b2369564c0b521e3f9ca2b
JUN JIHAN <jun_jihan@aitec-grp.co.jp>
2025-12-17 10:43:51 +0900

    Setting 화면 수정중

---
+++ DEMO2/Manual/Setting/SystemPoint.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DEMO2.Manual.Setting
+{
+    public partial class SystemPoint : UserControl
+    {
+        public SystemPoint()
+        {
+            InitializeComponent();
+        }
+    }
+}
